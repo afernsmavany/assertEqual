81dfb8e54a0a22a1d81ef12890757ad436ecca5b
afernsmavany <asifmavany@hotmail.com>
2019-07-29 20:47:37 +0000

    updated assertEqual with export

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,14 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return " :-) :-) Assertion Passed: (actual) === (expected)";
+    console.log(" :-) :-) Assertion Passed: (actual) === (expected)");
   }   else {
-    return ":-( :-( Assertion Failed: (actual) !== (expected)";
+    console.log(":-( :-( Assertion Failed: (actual) !== (expected)");
   }
 };
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual(2, 1));
-console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
+// console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+// console.log(assertEqual(1, 1));
+// console.log(assertEqual(2, 1));
+// console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
+
+module.exports = assertEqual;
