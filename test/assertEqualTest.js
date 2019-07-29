f9b165a0eae5e997d6ebaa3e45982914c5addf72
afernsmavany <asifmavany@hotmail.com>
2019-07-29 20:48:26 +0000

    assertEqualTest

---
+++ test/assertEqualTest.js
@@ -0,0 +1,17 @@
+ 
+ const assertEqual = require('../assertEqual');
+
+//  if (actual === expected) {
+//     return " :-) :-) Assertion Passed: (actual) === (expected)";
+//   }   else {
+//     return ":-( :-( Assertion Failed: (actual) !== (expected)";
+//   }
+// };
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("test", "test");
+assertEqual(1, 2);
+// console.log(assertEqual(1, 1));
+// console.log(assertEqual(2, 1));
+// console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
