f1e5b915aa2faa36ab54654e0493496a17bdd2ae
afernsmavany <asifmavany@hotmail.com>
2019-07-29 20:44:43 +0000

    updated head with export

--- head.js
+++ head.js
@@ -1,16 +1,19 @@
+const assertEqual = require('./assertEqual');
 
 const head = function(inputArray) {
   var actual =  inputArray[0];  
   return actual; 
   };
   
-  const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      return " :-) :-) Assertion Passed: (actual) === (expected)";
-    }   else {
-      return ":-( :-( Assertion Failed: (actual) !== (expected)";
-    }
-  };
+  // const assertEqual = function(actual, expected) {
+  //   if (actual === expected) {
+  //     return " :-) :-) Assertion Passed: (actual) === (expected)";
+  //   }   else {
+  //     return ":-( :-( Assertion Failed: (actual) !== (expected)";
+  //   }
+  // };
+
+  module.exports = head;
   
-  console.log(assertEqual(head([5,6,7]), 5));
-  console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+  //console.log(assertEqual(head([5,6,7]), 5));
+  //console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
